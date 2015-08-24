538318603dbb32cf5f1a08a60b50c6dc00101764
Jonathon Carlson <jon@jc2dev.com>
2015-08-24 16:58:55 -0600

    added basic function for creating recipe, django default form styling only

--- main/forms.py
+++ main/forms.py
@@ -1,3 +1,15 @@
 from django.forms import ModelForm
+from main.models import Recipe, Ingredient
 
-# Create your forms here.
+
+class RecipeForm(ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ['name', 'instructions', 'notes', 'source', 'rating',
+                  'difficulty', 'servings']
+
+
+class IngredientForm(ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = ['name', 'unit', 'quantity']
--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.forms.formsets import formset_factory
+from main.forms import RecipeForm, IngredientForm
 
-# Create your views here.
+
+def create_recipe(request):
+    IngredientFormSet = formset_factory(IngredientForm)
+    context = {}
+    context['form'] = RecipeForm
+    context['ingr'] = IngredientFormSet
+    if request.method == 'POST':
+        # TODO - add in the logic to separate the ingredients from the recipe
+        # then save the ingredients and the recipe.
+        #  The rest of this function is an example
+        formset = IngredientFormSet(request.POST, request.FILES)
+        if formset.is_valid():
+            # manipulate the data or save it or whatver....
+            pass
+        else:
+            formset = IngredientFormSet()
+    return render(request, 'main/create-recipe.html', context)
