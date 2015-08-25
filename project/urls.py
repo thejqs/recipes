819ed31f12050f4a6b9e8609927044172af95336
Peter Henry <phenry@caltech.edu>
2015-08-25 14:02:51 -0600

    working password reset (slow!)

--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.contrib.auth.views import (
+    password_reset,
+    password_reset_done,
+    password_reset_confirm,
+    password_reset_complete,
+)
+
 from main.views import (
     RegisterView,
     LoginView,
@@ -30,4 +37,23 @@ urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
 
-    url(r'^logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^password/reset/$',
+        password_reset,
+        {'template_name': 'project/password_reset.html'},
+        name='password_reset'),
+
+    url(r'^password/reset/done$',
+        password_reset_done,
+        {'template_name': 'project/password_reset_done.html'},
+        name='password_reset_done'),
+
+    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
+        password_reset_confirm,
+        {'template_name': 'project/password_reset_confirm.html'},
+        name='password_reset_confirm'),
+
+    url(r'^password/reset/confirm/done$',
+        password_reset_complete,
+        {'template_name': 'project/password_reset_complete.html'},
+        name='password_reset_complete'),
+]
