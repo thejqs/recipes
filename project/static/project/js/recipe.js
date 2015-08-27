8d75e117d6affd2607433098f3d5c0dd3c5ae6bd
Jonathon Carlson <jon@jc2dev.com>
2015-08-27 15:05:53 -0600

    fixed the ingredient form, can now dynamically add up to 1,000 ingredients

--- project/static/project/js/recipe.js
+++ project/static/project/js/recipe.js
@@ -1,12 +1,22 @@
 'user strict';
 
 
-// This is an attempt to dynamically load more ingredient fields.
-// it isn't fully functional yet, it does increment the page's form count
-// but it doesn't add more forms to the screen
-$('#create-recipe-form').on('click', '#add-ingredient', function(e){
-	e.preventDefault();
-	var forms = parseInt($('#id_form-TOTAL_FORMS').val(), 10);
-	forms += 1
-	$('#id_form-TOTAL_FORMS').val(forms);
-});
+// This adds more form entry fields for the ingredients onto the
+// create-recipe form. Then it increments the form count so that
+// the view accepts the new total number of forms for processing
+$(document).ready(function(){
+    $('.add-ingredient').click(function(e){
+        e.preventDefault();
+        var count = $('#items-form-container').children().length;
+        // gets the template in script tags at the bottom of the page
+        var tmplMarkup = $('#item-template').html();
+        // replaces the prefix value with the correct form number
+        var compiledTmpl = tmplMarkup.replace(/__prefix__/g, count);
+        $('div#items-form-container').append(compiledTmpl);
+
+        // updates the total form count
+        var forms = parseInt($('#id_form-TOTAL_FORMS').val(), 10);
+		forms += 1
+		$('#id_form-TOTAL_FORMS').val(forms);
+    })
+})
